b0bbc06a4383f9e52591592a166b7c3e133a8761
DIOGO DA SILVA <diogo_silva8@estudante.sesisenai.org.br>
2025-07-01 14:46:38 -0300

    url railway hospedando o backend adicionado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://sabackend-production.up.railway.app/api",
   withCredentials: true,
   timeout: 10000,
 })
